ddd8e6ee192be0d4031ac951284cced74cb20a3c
Daniel Kiss <flowfast47@gmail.com>
2023-07-21 10:16:03 -0700

    make testing and Order model associated with them

---
+++ VendorManager.Tests/ModelTests/OrdderTests.cs
@@ -0,0 +1,77 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorManager.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderManager.Tests
+{
+    [TestClass]
+    public class OrderTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Order.ClearAll();
+        }
+
+        [TestMethod]
+        public void OrderConstructor_CreateInstanceOfOrder_Order()
+        {
+            string title = "Order for Amy";
+            string description = "Dozen cookies";
+            int price = 10;
+            Order newOrder = new Order(title, description, price);
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+        [TestMethod]
+        public void GetTitle_CanReturnName_String()
+        {
+            string title = "Order for Amy";
+            string description = "Dozen cookies";
+            int price = 10;
+            Order newOrder = new Order(title, description, price);
+            Assert.AreEqual(title, newOrder.Title);
+        }
+
+        [TestMethod]
+        public void GetDescription_CanReturnDescription_String()
+        {
+            string title = "Order for Amy";
+            string description = "Dozen cookies";
+            int price = 10;
+            Order newOrder = new Order(title, description, price);
+            Assert.AreEqual(description, newOrder.Description);
+        }
+
+        [TestMethod]
+        public void GetId_CanReturnId_Int()
+        {
+            string title = "Order for Amy";
+            string description = "Dozen cookies";
+            int price = 10;
+            Order newOrder = new Order(title, description, price);
+            string title2 = "Order for Amy";
+            string description2 = "Dozen cookies";
+            int price2 = 20;
+            Order newOrder2 = new Order(title2, description2, price2);
+            int expectedId = 2;
+            Assert.AreEqual(expectedId, newOrder2.Id);
+        }
+
+        [TestMethod]
+        public void GetAll_CanReturnListOfAll_ListOfOrders()
+        {
+            string title = "Order for Amy";
+            string description = "Dozen cookies";
+            int price = 10;
+            Order newOrder = new Order(title, description, price);
+            string title2 = "Order for Amy";
+            string description2 = "Dozen cookies";
+            int price2 = 20;
+            Order newOrder2 = new Order(title2, description2, price2);
+
+            List<Order> expectedValues = new List<Order> { newOrder, newOrder2 };
+            CollectionAssert.AreEqual(expectedValues, Order.GetAll());
+        }
+    }
+}
---
+++ VendorManager/Models/Order.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System;
+
+namespace VendorManager.Models
+{
+    public class Order
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Date { get; }
+
+        public int Id { get; }
+
+        public static List<Order> _instances = new List<Order> { };
+
+        public Order(string title, string description, int price)
+        {
+            Title = title;
+            Description = description;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Price = price;
+            Date = DateTime.Now;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public static List<Order> GetAll()
+        {
+            return _instances;
+        }
+    }
+}
