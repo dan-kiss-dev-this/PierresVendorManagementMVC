727058faffe918821352f4cbf1b783177a8275f4
Daniel Kiss <flowfast47@gmail.com>
2023-07-21 16:43:50 -0700

    add way to show individual order in view to controller

--- VendorManager/Controllers/OrderController.cs
+++ VendorManager/Controllers/OrderController.cs
@@ -14,5 +14,16 @@ namespace VendorManager.Controllers
             return View(foundVendor);
         }
 
+        [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+        public ActionResult Show(int vendorId, int orderId)
+        {
+            Vendor foundVendor = Vendor.Find(vendorId);
+            Order foundOrder = Order.Find(orderId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("vendor", foundVendor);
+            model.Add("order", foundOrder);
+            return View(model);
+        }
+
     }
 }
